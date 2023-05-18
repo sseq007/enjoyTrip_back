956e151e2cc4c82b9bcd523521cac8f4405e88db
신준호 <sseq007@naver.com>
2023-05-18 17:55:44 +0900

    김주하 화이팅

--- src/main/java/com/ssafy/tripApp/board/controller/api/TripPartnerApiController.java
+++ src/main/java/com/ssafy/tripApp/board/controller/api/TripPartnerApiController.java
@@ -1,11 +1,16 @@
 package com.ssafy.tripApp.board.controller.api;
 
+import java.io.File;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpSession;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +26,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.ssafy.tripApp.board.ResponseDto;
@@ -31,6 +37,7 @@ import com.ssafy.tripApp.board.Sharetravel.service.ShareTravelService;
 import com.ssafy.tripApp.board.travelPlan.dto.TravelplanDto;
 import com.ssafy.tripApp.board.trippartner.dto.TripPartnerDto;
 import com.ssafy.tripApp.board.trippartner.service.TripPartnerService;
+import com.ssafy.tripApp.member.controller.MemberController;
 import com.ssafy.tripApp.member.dto.MemberDto;
 
 import lombok.RequiredArgsConstructor;
@@ -41,14 +48,18 @@ import lombok.RequiredArgsConstructor;
 @CrossOrigin("*")
 public class TripPartnerApiController {
 
+	private final Logger logger = LoggerFactory.getLogger(TripPartnerApiController.class);
 	private final TripPartnerService trippartnerService;
 	
-	
+	@Autowired
+	private ServletContext servletContext;
 	
 	@GetMapping("/list")
 	public ResponseEntity<?> list(@RequestParam Map<String, String> map) throws Exception {
 		ModelAndView mav = new ModelAndView();
 		List<TripPartnerDto> listPartner = trippartnerService.listPartner(map);
+		
+		
 //		System.out.println(listShare.toString());
 		mav.addObject("pgno", map.get("pgno"));
 		mav.addObject("key", map.get("key"));
@@ -57,14 +68,40 @@ public class TripPartnerApiController {
 		return new ResponseEntity<List<TripPartnerDto>>(listPartner, HttpStatus.OK);
 	}
 	@PostMapping("/write")
-	public ResponseEntity<?>write(@RequestBody TripPartnerDto trippartnerDto,HttpSession session){
-		
+	public ResponseEntity<?>write(@RequestParam("userId")String userId, @RequestParam("subject")String subject,@RequestParam("content")String content,@RequestParam("partnerObject")String partnerObject,@RequestParam("partnerCount")String partnerCount,@RequestParam("startDate")String startDate,@RequestParam("endDate")String endDate,@RequestParam("file") MultipartFile file){
+		TripPartnerDto trippartnerDto = new TripPartnerDto();
 		try {
+			if(!file.isEmpty()) {
+				String realPath = "C:/junho/enjoy_trip_vue/src/assets/img/partnerimg";
+				String saveFolder = realPath;
+				String saveFileName;
+				logger.debug("저장 폴더 : {}", saveFolder);
+				File folder = new File(saveFolder);
+				if (!folder.exists()) folder.mkdirs();
+				String origin = file.getOriginalFilename();
+				if(!origin.isEmpty()) {
+					saveFileName = UUID.randomUUID().toString()
+							+ origin;
+					logger.debug("원본 파일 이름 : {}, 실제 저장 파일 이름 : {}", origin, saveFileName);
+					file.transferTo(new File(folder, saveFileName));
+					trippartnerDto.setPartnerImage(saveFileName);
+				}
+				trippartnerDto.setLocation("다시짜야돼");
+				trippartnerDto.setPartnerUrl(saveFolder);
+				trippartnerDto.setUserId(userId);
+				trippartnerDto.setSubject(subject);
+				trippartnerDto.setContent(content);
+				trippartnerDto.setPartnerObject(partnerObject);
+				trippartnerDto.setPartnerCount(partnerCount);
+				trippartnerDto.setStartDate(startDate);
+				trippartnerDto.setEndDate(endDate);
+				
+			}
+			trippartnerService.writePartner(trippartnerDto);
 //			MemberDto memberDto = (MemberDto) session.getAttribute("userinfo");
 //			sharetravelDto.setUserId(memberDto.getUserId());
 			System.out.println(trippartnerDto.toString());
-			trippartnerService.writePartner(trippartnerDto);
-		} catch (SQLException e) {
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
